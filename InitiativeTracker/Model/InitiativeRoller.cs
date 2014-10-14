7332a29c502ec32aa3e53dbfc14091ac7bb6156e
Alexander Parks <alec.parks@gmail.com>
2014-10-14 15:35:01 -0500

    InitiativeRoller
    Class to roll and return an initiative given a combatant.

---
+++ InitiativeTracker/Model/InitiativeRoller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InitiativeTracker.Model
+{
+    static class InitiativeRoller
+    {
+        Random icosahedron = new Random();
+
+        public int RollInitiativeFor(Combatant combatant)
+        {
+            return icosahedron.Next(1, 21) + combatant.DexModifier;
+        }
+    }
+}
