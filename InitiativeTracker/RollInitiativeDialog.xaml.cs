279283261fc333822e0ce202eb59c709171b18ef
Alexander Parks <alec.parks@gmail.com>
2014-10-18 11:24:35 -0500

    Roll Initiative Dialog
    Set up exits for Roll Initiative.

--- InitiativeTracker/RollInitiativeDialog.xaml.cs
+++ InitiativeTracker/RollInitiativeDialog.xaml.cs
@@ -13,5 +13,23 @@ namespace InitiativeTracker
         {
             InitializeComponent();
         }
+
+        private void MonsterRoll_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void BothRoll_Click(object sender, RoutedEventArgs e)
+        {
+            players = true;
+            DialogResult = true;
+        }
+
+        private void PlayerRoll_Click(object sender, RoutedEventArgs e)
+        {
+            monsters = false;
+            players = true;
+            DialogResult = true;
+        }
     }
 }
