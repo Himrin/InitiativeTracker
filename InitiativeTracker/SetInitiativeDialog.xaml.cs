01cfb4463ba215b5e36163166bd596bd8ca4aa07
Alexander Parks <alec.parks@gmail.com>
2014-10-18 13:53:16 -0500

    Set Initiative
    Initial Dialog for setting initiative manually.

---
+++ InitiativeTracker/SetInitiativeDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Assisticant.Collections;
+using InitiativeTracker.Model;
+
+namespace InitiativeTracker
+{
+	/// <summary>
+	/// Interaction logic for SetInitiativeDialog.xaml
+	/// </summary>
+	public partial class SetInitiativeDialog : Window
+	{
+		public SetInitiativeDialog(ObservableList<Combatant> combatants )
+		{
+			InitializeComponent();
+		    InitiativeDisplayListBox.ItemsSource = combatants;
+		}
+	}
+}
